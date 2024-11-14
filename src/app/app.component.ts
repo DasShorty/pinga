ef789ece0396d6d4511d5639110f2044dcc86d37
Anthony T. <ungegrieftfirma@gmail.com>
2024-11-14 16:41:18 +0100

    add stuff?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NgForOf],
+  template: `
+  <div>
+    <div *ngFor="let service of services">
+
+      <h2>{{service.serviceName}}</h2>
+      <p>{{service.shortName}}</p>
+      <p>{{service.requestUri}}</p>
+
+      <hr>
+
+    </div>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'pinga';
+
+  services: any = null;
+
+  constructor(private httpClient: HttpClient) {
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer hello`,
+      Accept: '*'
+    })
+
+    httpClient.get("http://localhost:8080/service", {
+      headers
+    })
+      .subscribe(response => {
+        this.services = response;
+        console.log(response);
+      })
+
+  }
+
 }
